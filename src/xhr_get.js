1135164e890554f92bb31820aed69d3f30415a05
Sean Roberts <sean.roberts90@gmail.com>
2014-09-06 22:49:21 -0400

    Add comming and the options parameter

--- src/xhr_get.js
+++ src/xhr_get.js
@@ -1,4 +1,23 @@
-function get( url, cbSuccess, cbFail){
+
+/**
+  Simple GET AJAX function
+
+  url = the url to send the GET request
+  options = (optional) object to discern what to do with the request
+    options.type === 'json' will have the response JSON.parse()'d
+  cbSuccess = the success callback will be passed the response or parsed data
+  cbFail = (optional) the callback for a failure in sending the request
+*/
+
+function get( url, options, cbSuccess, cbFail){
+
+  // Check for optional options object
+  // and fix argument assignment if it is omitted
+  if( typeof options === 'function' ){
+    cbFail = cbSuccess;
+    cbSuccess = options;
+    options = {};
+  }
 
   var httpRequest;
 
@@ -22,7 +41,7 @@ function get( url, cbSuccess, cbFail){
   httpRequest.onreadystatechange = function(xhr){
     if (httpRequest.readyState === 4) {
       if(httpRequest.status === 200){
-        cbSuccess && cbSuccess(xhr);
+        cbSuccess( options.type === 'json' ? JSON.parse(xhr) : xhr);
       }else {
         console.error('Unable to fetch apps');
         cbFail && cbFail();
